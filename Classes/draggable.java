a74d1f85908607bfdc7d00549fd3adb69104d6b9
DarkMed15 <101574509+DarkMed15@users.noreply.github.com>
2022-03-21 10:32:37 -0400

    Add files via upload

--- Classes/draggable.java
+++ Classes/draggable.java
@@ -4,10 +4,22 @@
  */
 package CircuitBuilder;
 
-/**
- *
- * @author A103master
- */
 public class draggable {
+   
+    private double mouseX;
+    private double mouseY;
     
+    public void makeDraggable(Node node){
+
+        node.setOnMousePressed(mouseEvent -> {
+            mouseX = mouseEvent.getX();
+            mouseY = mouseEvent.getY();
+        });
+
+        node.setOnMouseDragged(mouseEvent -> {
+            node.setLayoutX(mouseEvent.getSceneX() - mouseX);
+            node.setLayoutY(mouseEvent.getSceneY() - mouseY);
+        });
 }
+}
+    
